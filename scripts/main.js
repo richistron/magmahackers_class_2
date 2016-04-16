05e742c5710b3d917459e3e96bad6b9c6134480d
Ricardo Rivas G <richistron@gmail.com>
2016-04-16 12:15:56 -0500

    Argh! About to give up :(

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 
-function YouTubeEmbeder(){
-   console.log('new video yay');
+function YouTubeEmbeder(options, callback){
+  this._setOptions(options);
+  // this.done(callback);
 }
 
 YouTubeEmbeder.prototype = {
-  getVideo: function(){},
-  getTemplate: function(){},
+
+  _setOptions: function(o){
+    if (o === undefined) {
+      return;
+    }
+    this._selector = o.selector || 'body';
+    this._videoId  = o.videoId || '';
+    this._styles   = o.styles || {};
+  },
+
+  getVideoId: function(){
+    return this._videoId;
+  },
+
+  getTemplate: function(){
+    return '<iframe width="560" height="315" src="https://www.youtube.com/embed/NJqXV_MW4aY" frameborder="0" allowfullscreen></iframe>';
+  },
+
   getComments: function(){},
+
   render: function(){}
 };
 
+
+// <iframe width="560" height="315" src="https://www.youtube.com/embed/NJqXV_MW4aY" frameborder="0" allowfullscreen></iframe>
 
--- spec/main.js
+++ spec/main.js
@@ -1,22 +1,40 @@
-
-
 describe('YouTubeEmbeder smoke test', function() {
-
-
   it('should exits', function(){
     expect(YouTubeEmbeder).toBeDefined();
   });
-
-
   it('should return and object', function(){
     expect(new YouTubeEmbeder()).toEqual(jasmine.any(Object));
   });
-
-
 });
 
-// describe('unit tests', function() {
-// });
+describe('unit tests', function() {
+  var proto = YouTubeEmbeder.prototype,
+  instance = new YouTubeEmbeder({
+    selector: 'body',
+    videoId: 'NJqXV_MW4aY',
+    styles: {
+      width: '100px',
+      height: '100px'
+    }
+  }, function(){
+      console.log('all done');
+  });
 
-// describe('BDD tests', function() {
-// });
+
+  it('unit getVideoId should return the video id', function(){
+    expect(proto.getVideoId).toBeDefined();
+    expect(instance.getVideoId()).toBeDefined('NJqXV_MW4aY');
+  });
+  it('unit getTemplate should return the iframe template', function(){
+    expect(proto.getTemplate).toBeDefined();
+    expect(instance.getTemplate()).toMatch('iframe');
+  }); it('unit getComments should return a list of comments', function(){
+    expect(proto.getComments).toBeDefined();
+  });
+  it('unit render should render the video template into the dom', function(){
+    expect(proto.render).toBeDefined();
+  });
+});
+
+describe('BDD tests', function() {
+});
